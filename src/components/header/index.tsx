c8b579823524b397d36c2658031084ea251eb8b3
Arthur <neiklotrr@gmail.com>
2023-06-09 12:25:13 +0300

    login and displaying all logs

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,24 @@
-import { LoginOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
+import { LoginOutlined, LogoutOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
 import styles from './index.module.css'
 import { Layout, Space } from 'antd'
 import CustomButton from '../customButton'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Paths } from '../../lib/paths'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout, selectUser } from '../../redux/slices/authSlice'
 
 const Header = () => {
+
+  const user = useSelector(selectUser)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const onLogout = () => {
+    dispatch(logout())
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
+
   return (
     <Layout.Header className={styles.header}>
         <Space>
@@ -16,14 +29,22 @@ const Header = () => {
             <h1>Rendezvous</h1>
           </Link>
         </Space>
-        <Space>
-          <Link to={Paths.register} className={styles.link}>
-            <CustomButton type='ghost' icon={<UserOutlined />}>Register</CustomButton>
-          </Link>
-          <Link to={Paths.login}>
-            <CustomButton type='default' icon={<LoginOutlined />}>Login</CustomButton>
-          </Link>
-        </Space>
+        {
+          user ? (
+            <CustomButton type='ghost' icon={<LogoutOutlined />} onClick={onLogout}>
+              Logout
+            </CustomButton>
+          ) : (
+            <Space>
+            <Link to={Paths.register} className={styles.link}>
+              <CustomButton type='ghost' icon={<UserOutlined />}>Register</CustomButton>
+            </Link>
+            <Link to={Paths.login}>
+              <CustomButton type='default' icon={<LoginOutlined />}>Login</CustomButton>
+            </Link>
+          </Space>
+          )    
+        }
     </Layout.Header>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,66 @@
+import { PlusCircleOutlined } from '@ant-design/icons'
+import CustomButton from '../../components/customButton'
+import Layout from '../../components/layout'
+import { Table } from 'antd'
+import { useGetAllLogsQuery } from '../../services/logs'
+import type { ColumnsType } from 'antd/es/table'
+import { Log } from '../../lib/types'
+import { useNavigate } from 'react-router-dom'
+import { Paths } from '../../lib/paths'
+import styles from './index.module.css'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../../redux/slices/authSlice'
+import { useEffect } from 'react'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+  const user = useSelector(selectUser)
+  const { data, isLoading } = useGetAllLogsQuery()
+
+  useEffect(() => {
+    if (!user) { 
+      navigate('/login')
+    }
+  }, [navigate, user])
+
+  const columns: ColumnsType<Log> = [
+    {
+      title: 'Name',
+      dataIndex: 'firstName',
+      key: 'firstName'
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age'
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address'
+    },
+  ]
+
   return (
-    <div>
-        Wagwan
-    </div>
+    <Layout>
+      <CustomButton type='primary' onClick={() => null} icon={<PlusCircleOutlined />}>
+        Add
+      </CustomButton>
+      <Table
+        className={styles.table} 
+        loading={isLoading} 
+        dataSource={data} 
+        pagination={false}
+        columns={columns}
+        rowKey={(record) => record.id}
+        onRow={(record) => {
+          return {
+            onClick: () => navigate(`${Paths.log}/${record.id}`)
+          }
+        }}
+        />
+    </Layout>
   )
 }
 
